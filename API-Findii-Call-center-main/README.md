752f97307c10ee7aea776b92feff2f49d8dd399c
Soporte Equiti Findii <159149139+soporteEqFi@users.noreply.github.com>
2024-02-13 22:10:35 -0500

    Delete API-Findii-Call-center-main directory

--- API-Findii-Call-center-main/README.md
+++
@@ -1,1 +0,0 @@
-# API-Findii-Call-center
--- API-Findii-Call-center-main/controladores/inicio_sesion/inicio_sesion_controlador.py
+++
@@ -1,9 +0,0 @@
-from modelos.inicio_sesion.inicio_sesion_modelo import *
-
-mod_inicio_sesion = Usuario()
-
-class IniciarSesionControlador():
-    
-    def inicio_sesion(self):
-        query = mod_inicio_sesion.login()
-        return query
